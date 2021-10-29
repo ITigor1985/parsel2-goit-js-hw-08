37ac65cdadbf3eee01b900fa262c0fb9f9a4cd96
Igor Tarasov <xromer85@gmail.com>
2021-10-29 21:15:24 +0300

    const

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const VIDEO_PLAYER_KEY = 'videoplayer-current-time';
+
 let timeUpdate = event => {
-  localStorage.setItem('videoplayer-current-time', event.seconds);
+  localStorage.setItem(VIDEO_PLAYER_KEY, event.seconds);
 };
 
 player.on('timeupdate', throttle(timeUpdate, 1000));
-const currentTime = localStorage.getItem('videoplayer-current-time');
+
+const currentTime = localStorage.getItem(VIDEO_PLAYER_KEY);
 if (currentTime) player.setCurrentTime(currentTime);
