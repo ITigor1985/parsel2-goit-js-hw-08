f0719855c65fd78dd27014c350c12062313de4ef
Igor Tarasov <xromer85@gmail.com>
2021-10-30 12:07:11 +0300

    456

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+const registrationData = {};
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(event.currentTarget);
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('все поля должны быть заполнены');
+  }
+  registrationData.email = email.value;
+  registrationData.message = message.value;
+
+  console.log(registrationData);
+  event.currentTarget.reset();
+}
